03279c3d3e29b8797233888680eb378e2972b9fb
Ty Piesco <typiesco@gmail.com>
2025-03-12 10:54:42 -0400

    improve tab names

--- src/modules/HeaderList.js
+++ src/modules/HeaderList.js
@@ -23,6 +23,43 @@ class HeaderList {
     headerList: document.querySelector(".open-modules-list"),
   };
 
+  // Ex: Silly Slots => Silly Slots
+  // Ex: Bamboozling Button Grid => Bam. Butt. Grid
+  shortenName(name) {
+    const MAX_TITLE_LEN = 15;
+
+    // Simple cases
+    if (name.length < MAX_TITLE_LEN) return name;
+    let nameParts = name.split(/[ -]/); // Split on (" " or "-")
+    if (nameParts.length === 1) {
+      return nameParts[0].slice(0, MAX_TITLE_LEN - 3) + "...";
+    }
+
+    // Advanced Cases (Needs Abbreviation)
+    nameParts = nameParts.map((item) => item.replace("and", "&"));
+    const ratio = Math.ceil(name.length / MAX_TITLE_LEN);
+    nameParts.map((word) => word.slice(0, word.length / ratio - 1));
+    nameParts.map((word) => word + ".");
+    let longestWordLength = Math.max(...nameParts.map((name) => name.length));
+    while (
+      nameParts.join(" ").length > MAX_TITLE_LEN ||
+      longestWordLength != 1
+    ) {
+      nameParts = nameParts.map((word) => {
+        if (nameParts.join(" ").length <= MAX_TITLE_LEN) {
+          return word; // Inside check to not cut off more letters than needed
+        } else if (word.length >= longestWordLength) {
+          console.log(word.slice(0, word.length - 2) + ".");
+          return word.slice(0, word.length - 2) + "."; // This word is the longest -> Trim it down 1 letter
+        }
+        return word; // There are longest words, trim those first.
+      });
+      console.log(nameParts);
+      longestWordLength--;
+    }
+    return nameParts.join(" ");
+  }
+
   addHeaderListItem(pubsubData) {
     const headerList = this.dom.headerList;
 
@@ -31,7 +68,8 @@ class HeaderList {
     newHeaderListItem.classList.add("open-module-list-item");
     newHeaderListItem.setAttribute("data-module-id", newHeaderListItemId);
     const headerText = document.createElement("span");
-    headerText.textContent = pubsubData.moduleName;
+    const moduleName = pubsubData.moduleName;
+    headerText.textContent = this.shortenName(moduleName);
     const tabClose = document.createElement("span");
     tabClose.classList.add("close-btn");
     tabClose.textContent = "x";
